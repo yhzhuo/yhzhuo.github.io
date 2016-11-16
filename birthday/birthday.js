490a04579d6c91ef0ca7e76d84885373b55be0a8
Ubuntu_14.04_64 <yaohuazhuo@gmail.com>
2016-11-15 21:52:54 -0800

    upload all files

---
+++ birthday/birthday.js
@@ -0,0 +1,13 @@
+var pictureNumber = 13;
+
+$(document).ready(function() {
+  console.log(1);
+  $("button#flip").click(function() {
+    console.log(2);
+    var date = new Date();
+    var selectedImage = (Math.floor((Math.random() * pictureNumber) + 1)) + ".png?reload=" + date.getTime();
+    console.log(3);
+    $("img#birthdayImage").attr("src", selectedImage);
+    console.log(4);
+  })
+});
